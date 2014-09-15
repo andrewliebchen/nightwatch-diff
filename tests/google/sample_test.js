59660474c49a6a7dd3e0b06ae1fdb3771d3062c8
andrewliebchen <andrewliebchen@gmail.com>
2014-09-15 00:38:19 -0700

    Get the base/ and tests/ folders to work

--- tests/google/sample_test.js
+++ tests/google/sample_test.js
@@ -1,19 +1,16 @@
 var fs = require('extfs');
 
-
-var empty = fs.isEmptySync("/screenshots/base");
-var screenshotPath = (typeof empty === 'undefined') ? 'screenshots/base/' : 'screenshots/test/';
-console.log(empty);
+var empty = fs.isEmptySync('./screenshots/base');
+var screenshotPath = empty ? 'screenshots/base/' : 'screenshots/test/';
+console.log('Adding screenshots to ' + screenshotPath);
 
 module.exports = {
-
-
   "Demo test Google" : function (client) {
     client
-      .url("http://www.google.com")
-      .waitForElementVisible("body", 1000)
-      .assert.title("Google")
-      // .saveScreenshot(screenshotPath + "1.png")
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .saveScreenshot(screenshotPath + '1.png')
       .end();
   }
 };
