5070fc1f2da3027e45a977039c7ca629a0453636
andrewliebchen <andrewliebchen@gmail.com>
2014-09-14 17:10:14 -0700

    Use Grunt to run Nightwatch and start/stop Selenium

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    nightwatch: {
+      options: {
+        "src_folders" : ["./tests"],
+        "output_folder" : "./reports",
+        "custom_commands_path" : "",
+        "custom_assertions_path" : "",
+        "globals_path" : "",
+
+        "selenium" : {
+          "start_process" : true,
+          "server_path" : "",
+          "log_path" : "",
+          "host" : "127.0.0.1",
+          "port" : 4444
+        },
+
+        "test_settings" : {
+          "default" : {
+            "launch_url" : "http://localhost",
+            "selenium_port"  : 4444,
+            "selenium_host"  : "localhost",
+            "silent": true,
+            "firefox_profile": false,
+            "chrome_driver": "",
+            "ie_driver": "",
+            "screenshots" : {
+              "enabled" : false,
+              "path" : ""
+            },
+            "desiredCapabilities": {
+              "browserName": "firefox",
+              "javascriptEnabled": true,
+              "acceptSslCerts": true
+            }
+          }
+        }
+      },
+
+      // test groups
+      google : {
+        "src_folders" : ["./tests/google"]
+      },
+      yahoo : {
+        "src_folders" : ["./tests/yahoo"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-nightwatch');
+  grunt.loadNpmTasks('grunt-selenium-webdriver');
+
+  grunt.registerTask('default', [
+    'selenium_start',
+    'nightwatch',
+    'selenium_stop'
+  ]);
+};
